f38c9563590379df71613abd11039f9aa36e5197
tuxafgmur <tuxafgmur@users.noreply.github.com>
2017-04-05 02:51:32 +0200

    Reduces quantity of useless tty devices

--- include/uapi/linux/vt.h
+++ include/uapi/linux/vt.h
@@ -6,9 +6,9 @@
  * These constants are also useful for user-level apps (e.g., VC
  * resizing).
  */
-#define MIN_NR_CONSOLES 1       /* must be at least 1 */
-#define MAX_NR_CONSOLES	63	/* serial lines start at 64 */
-#define MAX_NR_USER_CONSOLES 63	/* must be root to allocate above this */
+#define MIN_NR_CONSOLES      1    /* must be at least 1 */
+#define MAX_NR_CONSOLES	     11	  /* serial lines start at 64 */
+#define MAX_NR_USER_CONSOLES 11	  /* must be root to allocate above this */
 		/* Note: the ioctl VT_GETSTATE does not work for
 		   consoles 16 and higher (since it returns a short) */
 
